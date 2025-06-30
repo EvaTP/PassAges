56c0f117ab96703ef573c16034efa5f2b5e6633d
EvaTP <evathpr@icloud.com>
2025-06-30 15:39:51 +0200

    Merge branch 'main' of github.com:EvaTP/PassAges

--- src/app/accueil/page.tsx
+++ src/app/accueil/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+// import styles from "../accueil/page.module.css";
+import { useState, useEffect } from "react";
+
 export default function Page(): React.ReactNode {
   return <p>hello</p>;
 }
