c964c89a785c5f33c30fa2e7c45eddcfa5cb8ed2
VitryAhmed <aayari97@gmail.com>
2025-07-01 13:53:22 +0200

    add components

---
+++ src/app/components/Moment-Type.tsx
@@ -0,0 +1,14 @@
+export default function MomentType(props: { label: string, moment: Array<{ label: string, value: string }> }) {
+    return (
+        <div>
+            <label>{props.label} </label>
+            <select>
+                {props.moment.map((option) => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
