f25fee88902eec2704efd3ceaf226610fba31779
EvaTP <evathpr@icloud.com>
2025-07-04 09:35:14 +0200

    create volunteers page

--- src/app/volunteers/page.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import Header from "@/app/components/Header";
-import Footer from "@/app/components/Footer/Footer";
-import { useState, useEffect } from "react";
-import { prisma } from "@/app/lib/prisma";
