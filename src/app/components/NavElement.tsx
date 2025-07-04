3b1cf08211e7814d5931c92c17e31915d0e9de5f
VitryAhmed <aayari97@gmail.com>
2025-07-04 09:29:38 +0200

    fix navbar

--- src/app/components/NavElement.tsx
+++ src/app/components/NavElement.tsx
@@ -1,4 +1,18 @@
 export default function NavElement (props: { icon: string, label: string,   }): React.ReactNode
 {
-    return (<a href="#"><img src={props.icon} /> {props.label}</a> )
+    return (  
+
+        <nav className="flex justify-between p-4  text-white">
+            <div className="flex items-center space-x-4">
+                
+                <ul className="flex space-x-4">
+                <li>
+                    <a href="#" className="hover:text-gray-400"><img src={props.icon}></img> {props.label}</a>
+                </li>
+                
+                </ul>
+            </div>
+        </nav>
+    )
 }
+
