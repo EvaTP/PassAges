6496a23f78f61f8131218fa30034eaa7bf41f328
EvaTP <evathpr@icloud.com>
2025-07-04 15:35:57 +0200

    fix volunteers page

--- src/app/volunteers/page.tsx
+++ src/app/volunteers/page.tsx
@@ -9,30 +9,33 @@ import { prisma } from "@/app/lib/prisma";
 import { WHY_VOLUNTEER } from "@/app/data/cards";
 
 export default function Volunteers() {
-  return <p>Volunteers page</p>;
-
-  <div>
-    <div>
-      <h2 className="text-center text-2xl font-bold mb-2">
-        POURQUOI DEVENIR BÉNÉVOLE ?
-      </h2>
-      <p className="text-center mt-2 mb-15">
-        Être bénévole chez Adaence, c'est bien plus qu'une simple activité.
-        C'est une expérience enrichissante qui apporte du sens à votre vie et à
-        celle des autres.
-      </p>
-      <div className="grid md:grid-cols-4 gap-6">
-        {WHY_VOLUNTEER.map((w, index) => (
-          <div
-            key={w.title || index}
-            className="bg-white p-6 rounded-xl shadow-md flex flex-col items-center text-center hover:shadow-lg transition-shadow duration-300"
-          >
-            <span className="text-4xl mb-2">{w.emoji}</span>
-            <h3 className="">{w.title}</h3>
-            <p className="text-sm text-gray-600">{w.text}</p>
+  return (
+    // <p>Volunteers page</p>;
+    <>
+      <div>
+        <div>
+          <h2 className="text-center text-2xl font-bold mb-2">
+            POURQUOI DEVENIR BÉNÉVOLE ?
+          </h2>
+          <p className="text-center mt-2 mb-15">
+            Être bénévole chez Adaence, c'est bien plus qu'une simple activité.
+            C'est une expérience enrichissante qui apporte du sens à votre vie
+            et à celle des autres.
+          </p>
+          <div className="grid md:grid-cols-4 gap-6">
+            {WHY_VOLUNTEER.map((w, index) => (
+              <div
+                key={w.title || index}
+                className="bg-white p-6 rounded-xl shadow-md flex flex-col items-center text-center hover:shadow-lg transition-shadow duration-300"
+              >
+                <span className="text-4xl mb-2">{w.emoji}</span>
+                <h3 className="">{w.title}</h3>
+                <p className="text-sm text-gray-600">{w.text}</p>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-    </div>
-  </div>;
+    </>
+  );
 }
