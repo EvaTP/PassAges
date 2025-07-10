8d3e3859b0c58f0739c3a3457f8d5e910fd7881e
EvaTP <evathpr@icloud.com>
2025-07-10 10:03:04 +0200

    update tables schema

--- src/app/components/Footer/LegalSection.tsx
+++ src/app/components/Footer/LegalSection.tsx
@@ -4,9 +4,9 @@ export const LegalSection: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="">
+    <div className="text-white">
       <p>&copy; {currentYear} PassAges. Tous droits réservés.</p>
-      <p className="text-center">
+      <p className="text-center text-white">
         Ce site a été développé par Ahmed Ayari et Eva Tharrats pour un projet
         école Ada Tech School.
       </p>
