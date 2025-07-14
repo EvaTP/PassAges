16e772f9a9781a0b80a3de6b67dd560031c7a41e
EvaTP <evathpr@icloud.com>
2025-07-14 11:54:56 +0200

    fix: yellow button

--- src/app/components/BlueButton.tsx
+++ src/app/components/BlueButton.tsx
@@ -1,6 +1,6 @@
 export default function BlueButton(props: { label: string }): React.ReactNode {
   return (
-    <button className="bg-[#8584ff] hover:bg-fuchsia-500 text-white text-lg font-bold">
+    <button className="bg-[#8584ff] hover:bg-[#ffc412] text-white text-lg font-bold">
       {props.label}
     </button>
   );
--- src/app/components/YellowButton.tsx
+++ src/app/components/YellowButton.tsx
@@ -5,7 +5,7 @@ export default function YellowButton(props: {
 }): React.ReactNode {
   return (
     <Link href="/volunteers">
-      <button className="bg-[#ffc412] hover:bg-[#8584ff] text-white text-lg text-bold px-6 py-4 rounded transition-colors">
+      <button className="bg-[#ffc412] hover:bg-[#8584ff] text-white text-lg font-bold px-6 py-4 rounded mb-4 transition-colors duration-200">
         {props.label}
       </button>
     </Link>
