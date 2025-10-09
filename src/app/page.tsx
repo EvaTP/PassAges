3b6adb26bc5d282551688f3a95e893ddf491ec43
EvaTP <evathpr@icloud.com>
2025-10-09 15:01:05 +0200

    fix: move home page to root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,140 @@
 import Image from "next/image";
+import { getEldersPicture } from "@/app/actions/getEldersPicture";
+import { HOW_IT_WORKS } from "@/app/data/cards";
+import { ElderPicture } from "@/app/types/elders";
+import MomentToShare from "@/app/components/MomentToShare";
+import BlueButton from "@/app/components/BlueButton";
+import YellowButton from "@/app/components/YellowButton";
+import BlackButtonVisit from "@/app/components/BlackButtonVisit";
 
-export default function Home() {
+export default async function Home() {
+  const elders: ElderPicture[] = await getEldersPicture();
+  // const router = useRouter();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <>
+      <div className="">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          className="w-full h-[290px] object-cover"
+          src="/images/hero-desktop.svg"
+          alt="hero-image"
           width={180}
-          height={38}
+          height={50}
           priority
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* Moments à partager*/}
+      <div>
+        <MomentToShare />
+      </div>
+
+      <main className="p-6 bg-gray-50 mt-10">
+        <h1>LES PERSONNALITÉS À RENCONTRER</h1>
+        <p className="text-center mt-1 mb-5 text-2xl">
+          Feuilletez les profils de celles et ceux qui attendent simplement un
+          peu de votre temps. <br />
+          Chaque sourire porte une histoire, chaque rencontre est une promesse.
+        </p>
+        <div className="mx-auto w-[80%] mt-10">
+          {/* BENTO GRID */}
+
+          <div className="grid grid-cols-2 md:grid-cols-4 auto-rows-[150px] md:auto-rows-[200px] gap-3">
+            {elders.slice(0, 8).map((elder, index) => {
+              let gridClasses = "";
+              switch (index) {
+                case 0:
+                  gridClasses = "col-start-1 row-span-2";
+                  break;
+                case 1:
+                  gridClasses = "col-start-1 row-start-3";
+                  break;
+                case 2:
+                  gridClasses = "col-start-2 row-start-1";
+                  break;
+                case 3:
+                  gridClasses = "col-start-2 row-span-2 row-start-2";
+                  break;
+                case 4:
+                  gridClasses = "col-start-3 row-span-2";
+                  break;
+                case 5:
+                  gridClasses = "col-start-3 row-start-3";
+                  break;
+                case 6:
+                  gridClasses = "col-start-4 row-start-1";
+                  break;
+                case 7:
+                  gridClasses = "col-start-4 row-span-2";
+                  break;
+                default:
+                  break;
+              }
+              return (
+                <div
+                  key={elder.id}
+                  className={`overflow-hidden rounded-sm w-full h-full ${gridClasses}`}
+                >
+                  {elder.picture ? (
+                    <Image
+                      src={elder.picture}
+                      alt={`Photo aîné #${elder.id}`}
+                      width={400}
+                      height={300}
+                      className="object-cover w-full h-full rounded-sm"
+                    />
+                  ) : (
+                    <div className="w-full h-full bg-gray-200 flex items-center justify-center rounded-sm">
+                      <span className="text-gray-600 text-sm">
+                        Pas d&apos;image
+                      </span>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="bg-rose-50 p-6 mt-20">
+          <h2 className="text-center text-2xl font-bold mb-2">
+            COMMENT ÇA MARCHE
+          </h2>
+          <p className="text-center mt-2 mb-15 text-xl">
+            Offrir un peu de votre temps, c&apos;est offrir beaucoup. Découvrez
+            comment planifier une visite en toute simplicité.
+          </p>
+          <div className="grid md:grid-cols-4 gap-6">
+            {HOW_IT_WORKS.map((w, index) => (
+              <div
+                key={w.title || index}
+                className="bg-white p-6 rounded-xl shadow-md flex flex-col items-center text-center hover:shadow-lg transition-shadow duration-300"
+              >
+                <span className="text-4xl mb-2">{w.emoji}</span>
+                <h3 className="">{w.title}</h3>
+                <p className="text-sm text-gray-600">{w.text}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h2 className="text-center text-2xl font-bold mb-4 mt-8">
+            PRÊT⸱E À FAIRE LA DIFFÉRENCE ?
+          </h2>
+          <p className="text-center mb-4">
+            Offrez un peu de votre temps, partagez un moment, créez du lien...
+            Chaque présence compte. <br />
+            En tendant la main à une personne âgée, vous lui offrez bien plus
+            qu&apos;une visite : une vraie bouffée de chaleur humaine.
+          </p>
+          <div className=" flex flex-row justify-center mt-10 mb-5 gap-12 content-center px-4">
+            <BlackButtonVisit label="Programmer une visite" />
+            <BlueButton label="Faire un don 🫶" />
+            <YellowButton label="Devenir bénévole" />
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
